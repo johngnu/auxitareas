f7e08d83d0d7149bcc8d4673fc27d38ddbf73186
John Castillo <john.gnu@gmail.com>
2022-09-02 14:03:00 -0400

    aa

--- multipilas/src/ejerviernes/PilaIES.java
+++ multipilas/src/ejerviernes/PilaIES.java
@@ -5,92 +5,77 @@ package ejerviernes;
  * @author helen castillo
  */
 public class PilaIES {
-     private int max=100;
-    private IEducSup v[]=new IEducSup[max+1];
+
+    private int max = 100;
+    private IEducSup v[] = new IEducSup[max + 1];
     private int tope;
-    
-    PilaIES()
-    {
-     tope=0;       
-    }
-    boolean esvacia ()
-    {
-	if (tope == 0)
-	    return (true);
-	return (false);
+
+    PilaIES() {
+        tope = 0;
     }
 
-
-    boolean esllena ()
-    {
-	if (tope == max)
-	    return (true);
-	return (false);
+    boolean esvacia() {
+        if (tope == 0) {
+            return (true);
+        }
+        return (false);
     }
 
-
-    int nroelem ()
-    {
-	return (tope);
+    boolean esllena() {
+        if (tope == max) {
+            return (true);
+        }
+        return (false);
     }
 
-
-    void adicionar (IEducSup elem)
-    {
-	if (!esllena ())
-	{
-	    tope++;
-	    v [tope] = elem;
-	}
-	else
-	    System.out.println ("Pila llena");
+    int nroelem() {
+        return (tope);
     }
 
-
-    IEducSup eliminar ()
-    {
-	IEducSup elem = null;
-	if (!esvacia ())
-	{
-	    elem = v [tope];
-	    tope--;
-	}
-	else
-	    System.out.println ("Pila vacia");
-	return (elem);
+    void adicionar(IEducSup elem) {
+        if (!esllena()) {
+            tope++;
+            v[tope] = elem;
+        } else {
+            System.out.println("Pila llena");
+        }
     }
 
-
-    void mostrar ()
-    {
-	IEducSup elem = new IEducSup();
-	if (esvacia ())
-	    System.out.println ("Pila vacia");
-	else
-	{
-	    System.out.println ("\n Datos de la Pila ");
-	    PilaIES aux = new PilaIES ();
-	    while (!esvacia ())
-	    {
-		elem = eliminar ();
-		aux.adicionar (elem);
-		elem.mostrar();
-	    }
-	    while (!aux.esvacia ())
-	    {
-		elem = aux.eliminar ();
-
-		adicionar (elem);
-	    }
-	}
+    IEducSup eliminar() {
+        IEducSup elem = null;
+        if (!esvacia()) {
+            elem = v[tope];
+            tope--;
+        } else {
+            System.out.println("Pila vacia");
+        }
+        return (elem);
     }
 
+    void mostrar() {
+        IEducSup elem = new IEducSup();
+        if (esvacia()) {
+            System.out.println("Pila vacia");
+        } else {
+            System.out.println("\n Datos de la Pila ");
+            PilaIES aux = new PilaIES();
+            while (!esvacia()) {
+                elem = eliminar();
+                aux.adicionar(elem);
+                elem.mostrar();
+            }
+            while (!aux.esvacia()) {
+                elem = aux.eliminar();
 
-    
-    void vaciar (PilaIES a)
-    {
-	while (!a.esvacia ())
-	    adicionar (a.eliminar ());
+                adicionar(elem);
+            }
+        }
+    }
+
+    void vaciar(PilaIES a) {
+        while (!a.esvacia()) {
+            adicionar(a.eliminar());
+        }
 
     }
 }
--- multipilas/src/ejerviernes/Principal.java
+++ multipilas/src/ejerviernes/Principal.java
@@ -1,7 +1,7 @@
 package ejerviernes;
 
 public class Principal {
-
+    
     public static void main(String[] args) {
         // TODO code application logic here
         PilaIES A = new PilaIES();
@@ -17,22 +17,22 @@ public class Principal {
         A.adicionar(e1);
         A.adicionar(e2);
         A.adicionar(e3);
-
+        
         PilaIES B = new PilaIES();
         B.adicionar(e4);
         B.adicionar(e5);
-
+        
         PilaIES C = new PilaIES();
         C.adicionar(e6);
         C.adicionar(e7);
         C.adicionar(e8);
         C.adicionar(e9);
-
+        
         ColaSZ Z = new ColaSZ();
         Zona z1 = new Zona("Centro", "Central", A);
         Zona z2 = new Zona("Cotahuma", "Llojeta", B);
         Zona z3 = new Zona("Sur", "Calacoto", C);
-
+        
         Z.adicionar(z1);
         Z.adicionar(z2);
         Z.adicionar(z3);
@@ -40,8 +40,10 @@ public class Principal {
 
         // 5
         mostrar(Z, "Calacoto");
+        
+        
     }
-
+    
     public static void mostrar(ColaSZ Z, String nombre) {
         Zona elem = null;
         if (Z.esvacia()) {
@@ -51,7 +53,7 @@ public class Principal {
             while (!Z.esvacia()) {
                 elem = Z.eliminar();
                 aux.adicionar(elem);
-                if(elem.getNombre().equals(nombre)) {
+                if (elem.getNombre().equals(nombre)) {
                     System.out.println("Instituciones de la zona: " + elem.getNombre());
                     elem.getC().mostrar();
                 }
@@ -60,6 +62,27 @@ public class Principal {
             while (!aux.esvacia()) {
                 elem = aux.eliminar();
                 Z.adicionar(elem);
+            }
+        }
+    }
+    
+    public static void mostrar(PilaIES A, ColaSZ Z, String nombre) {
+        IEducSup elem = new IEducSup();
+        if (A.esvacia()) {
+            System.out.println("Pila vacia");
+        } else {
+            System.out.println("\n Institucion: " + nombre);
+            PilaIES aux = new PilaIES();
+            while (!A.esvacia()) {
+                elem = A.eliminar();
+                aux.adicionar(elem);
+                if (nombre.equals(elem.getNombre())) {
+                    
+                }
+            }
+            while (!aux.esvacia()) {
+                elem = aux.eliminar();
+                A.adicionar(elem);
             }
         }
     }
